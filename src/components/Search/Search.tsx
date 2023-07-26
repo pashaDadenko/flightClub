b5e689e8789194be4b9f70c084b1b43b3b20e5ba
Your Name <you@example.com>
2023-07-26 23:42:20 +0300

    12

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -6,9 +6,13 @@ import SearchIcon from '@mui/icons-material/Search';
 import styles from './Search.module.scss';
 
 export const Search: FC<SearchProps> = ({ setIsClicked }) => {
+	const handleInputClick = () => {
+		setIsClicked(true);
+	};
+
 	return (
 		<div className={styles.wrapper}>
-			<input onClick={() => setIsClicked(true)} className={styles.search} type='search' placeholder='Search' />
+			<input onClick={() => handleInputClick()} className={styles.search} type='search' placeholder='Search' />
 			<SearchIcon className={styles.icon} />
 		</div>
 	);
--- src/components/SearchOpen/SearchOpen.tsx
+++ src/components/SearchOpen/SearchOpen.tsx
@@ -12,6 +12,7 @@ export const SearchOpen: FC<SearchProps> = ({ setIsClicked }) => {
 	const sneakersData = useSelector((state: RootState) => state.sneakersDataSlice.sneakersData);
 
 	const [searchValue, setSearchValue] = useState('');
+
 	const inputRef = useRef<HTMLInputElement>(null);
 
 	const searchClear = () => {
@@ -26,75 +27,78 @@ export const SearchOpen: FC<SearchProps> = ({ setIsClicked }) => {
 	});
 
 	return (
-		<div className={styles.wrapper}>
-			<div className={styles.wrapSearch}>
-				<SearchIcon className={styles.icon} />
-				<input
-					type='search'
-					ref={inputRef}
-					value={searchValue}
-					placeholder='Search'
-					className={styles.search}
-					onChange={(e) => setSearchValue(e.target.value)}
-				/>
-				{searchValue && (
-					<button className={styles.buttonClear} onClick={() => searchClear()}>
-						Clear
+		<>
+			<div className={styles.wrapper}>
+				<div className={styles.wrapSearch}>
+					<SearchIcon className={styles.icon} />
+					<input
+						type='search'
+						ref={inputRef}
+						value={searchValue}
+						placeholder='Search'
+						className={styles.search}
+						onChange={(e) => setSearchValue(e.target.value)}
+					/>
+					{searchValue && (
+						<button className={styles.buttonClear} onClick={() => searchClear()}>
+							Clear
+						</button>
+					)}
+					<div className={styles.line}></div>
+					<button className={styles.buttonClose} onClick={() => setIsClicked(false)}>
+						Close
 					</button>
-				)}
-				<div className={styles.line}></div>
-				<button className={styles.buttonClose} onClick={() => setIsClicked(false)}>
-					Close
-				</button>
-			</div>
-
-			<div className={styles.wrapSearchBottom}>
-				<div className={styles.left}>
-					<p className={styles.title}>Popular</p>
-					<ul className={styles.previewGroupe}>
-						{filteredSneakers &&
-							filteredSneakers.slice(0, 8).map((sneaker) => (
-								<Link to={`/details/${sneaker.id}`} key={sneaker.id} className={styles.previewProduct}>
-									<img className={styles.img} src={sneaker.images[0]} alt='image' />
-									<p className={styles.text}>{sneaker.title}</p>
-								</Link>
-							))}
-					</ul>
 				</div>
-				<div className={styles.right}>
-					<p className={styles.title}>Categories</p>
-					<div className={styles.wrap}>
-						<div className={styles.brand}>
-							<Link to={'/air-jordan'}>
-								<button className={styles.button}>Air jordan</button>
-							</Link>
-							<Link to={'/new-balance'}>
-								<button className={styles.button}>New Balance</button>
-							</Link>
-							<Link to={'/nike'}>
-								<button className={styles.button}>Nike</button>
-							</Link>
-							<Link to={'/off-white'}>
-								<button className={styles.button}>Off white</button>
-							</Link>
-							<Link to={'/yeezy'}>
-								<button className={styles.button}>Yeezy</button>
-							</Link>
-							<Link to={'/sneakers'}>
-								<button className={styles.button}>More Brands</button>
-							</Link>
-						</div>
-						<div className={styles.recommended}>
-							<Link to={'/top-sellers'}>
-								<button className={styles.button}>Top sellers</button>
-							</Link>
-							<Link to={'/lowest-price'}>
-								<button className={styles.button}>Lowest price</button>
-							</Link>
+
+				<div className={styles.wrapSearchBottom}>
+					<div className={styles.left}>
+						{filteredSneakers.length > 0 ? <p className={styles.title}>Popular</p> : <p className={styles.title}>No Result</p>}
+						<ul className={styles.previewGroupe}>
+							{filteredSneakers &&
+								filteredSneakers.slice(0, 8).map((sneaker) => (
+									<Link to={`/details/${sneaker.id}`} key={sneaker.id} className={styles.previewProduct}>
+										<img className={styles.img} src={sneaker.images[0]} alt='image' />
+										<p className={styles.text}>{sneaker.title}</p>
+									</Link>
+								))}
+						</ul>
+					</div>
+					<div className={styles.right}>
+						<p className={styles.title}>Categories</p>
+						<div className={styles.wrap}>
+							<div className={styles.brand}>
+								<Link to={'/air-jordan'}>
+									<button className={styles.button}>Air jordan</button>
+								</Link>
+								<Link to={'/new-balance'}>
+									<button className={styles.button}>New Balance</button>
+								</Link>
+								<Link to={'/nike'}>
+									<button className={styles.button}>Nike</button>
+								</Link>
+								<Link to={'/off-white'}>
+									<button className={styles.button}>Off white</button>
+								</Link>
+								<Link to={'/yeezy'}>
+									<button className={styles.button}>Yeezy</button>
+								</Link>
+								<Link to={'/sneakers'}>
+									<button className={styles.button}>More Brands</button>
+								</Link>
+							</div>
+							<div className={styles.recommended}>
+								<Link to={'/top-sellers'}>
+									<button className={styles.button}>Top sellers</button>
+								</Link>
+								<Link to={'/lowest-price'}>
+									<button className={styles.button}>Lowest price</button>
+								</Link>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+			<div className={styles.overlay} onClick={() => setIsClicked(false)}></div>
+		</>
 	);
 };
