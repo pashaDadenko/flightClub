164c22585214f67a9a12d961650f18d65aeb7f01
Your Name <you@example.com>
2023-10-01 20:43:16 +0300

    swiper done

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,27 +1,50 @@
-import { FC, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { FC, useRef, useState } from 'react';
 import { RootState } from '../../redux/store';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 
 import styles from './Carousel.module.scss';
+import 'swiper/css';
 
 export const Carousel: FC = () => {
+	const swiperRef = useRef<any>(null);
 	const imageCarousel = useSelector((state: RootState) => state.sneakersSlice.ImageCarousel);
-	const [currentImage, setCurrentImage] = useState<number>(0);
 
-	const handleBefore = () => setCurrentImage((prevImage) => (prevImage === 0 ? imageCarousel.length - 1 : prevImage - 1));
-	const handleNext = () => setCurrentImage((prevImage) => (prevImage === imageCarousel.length - 1 ? 0 : prevImage + 1));
+	const goNext = () => swiperRef.current.slideNext();
+	const goPrev = () => swiperRef.current.slidePrev();
+
+	const [isFirstSlide, setIsFirstSlide] = useState(true);
+	const [isLastSlide, setIsLastSlide] = useState(false);
+
+	const handleSlideChange = (swiper: any) => {
+		setIsFirstSlide(swiper.isBeginning);
+		setIsLastSlide(swiper.isEnd);
+	};
 
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.carousel}>
-				<NavigateBeforeIcon className={styles.arrow} onClick={handleBefore} />
-				<img className={styles.image} src={imageCarousel[currentImage]} alt={`Slide ${currentImage}`} />
-				<NavigateNextIcon className={styles.arrow} onClick={handleNext} />
+				<NavigateBeforeIcon className={`${styles.arrow} ${isFirstSlide ? styles.opacity : ''}`} onClick={goPrev} />
+				<Swiper
+					allowTouchMove={false}
+					slidesPerView={1}
+					speed={1000}
+					onSwiper={(swiper) => {
+						swiperRef.current = swiper;
+						handleSlideChange(swiper);
+					}}
+					onSlideChange={(swiper) => handleSlideChange(swiper)}>
+					{imageCarousel &&
+						imageCarousel.map((image, index) => (
+							<SwiperSlide key={index}>
+								<img className={styles.image} src={image} alt='image' />
+							</SwiperSlide>
+						))}
+				</Swiper>
+				<NavigateNextIcon className={`${styles.arrow} ${isLastSlide ? styles.opacity : ''}`} onClick={goNext} />
 			</div>
 		</div>
 	);
 };
-
-// на mobile сделать swiper
