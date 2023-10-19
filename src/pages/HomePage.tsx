f31aec7193af41aa047806cef943964b0fa8144f
Your Name <you@example.com>
2023-10-19 23:44:32 +0300

    fix loader

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 import { Api } from '../api/Api';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import { MoonLoader } from 'react-spinners';
 import { Header } from '../components/Header/Header';
 import { Footer } from '../components/Footer/Footer';
 import { InfoBlock } from '../components/InfoBlock/InfoBlock';
@@ -8,17 +11,27 @@ import { AirJordanBlock } from '../components/AirJordanBlock/AirJordanBlock';
 import { TopSellersBlock } from '../components/TopSellersBlock/TopSellersBlock';
 
 export const HomePage: FC = () => {
+	const allSneakers = useSelector((state: RootState) => state.sneakersSlice.allSneakers);
+
 	Api();
 	window.scrollTo(0, 0);
 
 	return (
 		<>
-			<Header />
-			<AirJordanBlock />
-			<OffWhiteBlock />
-			<TopSellersBlock />
-			<InfoBlock />
-			<Footer />
+			{allSneakers.length > 0 ? (
+				<>
+					<Header />
+					<AirJordanBlock />
+					<OffWhiteBlock />
+					<TopSellersBlock />
+					<InfoBlock />
+					<Footer />
+				</>
+			) : (
+				<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100vw', height: '100vh' }}>
+					<MoonLoader color='#e91c29' size={150} speedMultiplier={1} />
+				</div>
+			)}
 		</>
 	);
 };
