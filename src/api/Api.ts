6dc0f7304af9cf27c4f5cb0c189adf9aa36e7b52
Your Name <you@example.com>
2023-10-19 23:46:10 +0300

    1

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -7,7 +7,7 @@ import { setSneakersData } from '../redux/sneakersSlice/sneakersSlice';
 export const Api = (): void => {
 	const dispatch = useDispatch();
 
-	const URL_DATA: string = `https://646cb4927b42c06c3b2bd66e.mockapi.io/sneakersData1`;
+	const URL_DATA: string = `https://646cb4927b42c06c3b2bd66e.mockapi.io/sneakersData`;
 
 	useEffect(() => {
 		const fetchData = async () => {
